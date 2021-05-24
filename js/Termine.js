cdf67682b0e46b398c17621bbc5abec2a80d201a
lcsnpmk <67631559+lcsnpmk@users.noreply.github.com>
2021-05-24 14:54:43 +0200

    Automatisierte Erstellung der Ausstehend-Spalten

--- js/Termine.js
+++ js/Termine.js
@@ -6,8 +6,10 @@ window.addEventListener("load", init);
 function init() {
     //speicherButton()
     termine = [];
+    ausstehendeImpfungen =[];
+
     if (window.localStorage !== null) {
-        holeTermine();
+        holeLocalStorage();
     }
     //Hier wird immer leer gesetzt, bei Abspeicherung im Browser muss hier auf Gespeichertes zurückgegriffen werden
     document.getElementById("terminAnlegen").addEventListener("keypress", function (e) { if (e.key.toLowerCase() == "enter") speicherButton(); });
@@ -21,6 +23,7 @@ function init() {
 function speicherButton() {
     var termin = new Object();
 
+
     termin.datum = document.getElementById("terminDatum").value; console.log(document.getElementById("terminDatum").value);
     termin.art = document.getElementById("terminArt").value; console.log(document.getElementById("terminArt").value);
     termin.arzt = document.getElementById("terminArzt").value; console.log(document.getElementById("terminArzt").value);
@@ -33,7 +36,7 @@ function speicherButton() {
 }
 
 function zeichneTermine() {
-    loescheTabelle();
+    loescheTerminTabelle();
 
     let table = document.getElementById("terminDaten");
     if (termine == "") {
@@ -45,46 +48,56 @@ function zeichneTermine() {
         terminDatum.classList.add("col-12");
         tr.appendChild(terminDatum);
         terminDatum.innerHTML = "Keine Termine";
-    }
+    }else{
 
-    for (let termin of termine) {
-        let tr = document.createElement("tr");
-        tr.classList.add("d-flex");
-        table.appendChild(tr);
+        for (let termin of termine) {
+            let tr = document.createElement("tr");
+            tr.classList.add("d-flex");
+            table.appendChild(tr);
 
-        let terminDatum = document.createElement("td");
-        terminDatum.classList.add("col-3");
-        tr.appendChild(terminDatum);
-        let terminDatumString = new Date(termin.datum)
-        terminDatum.innerHTML = terminDatumString.toLocaleDateString('de-DE');
+            let terminDatum = document.createElement("td");
+            terminDatum.classList.add("col-3");
+            tr.appendChild(terminDatum);
+            let terminDatumString = new Date(termin.datum)
+            terminDatum.innerHTML = terminDatumString.toLocaleDateString('de-DE');
 
-        let terminArt = document.createElement("td");
-        terminArt.classList.add("col-3");
-        tr.appendChild(terminArt);
-        terminArt.innerHTML = termin.art;
+            let terminArt = document.createElement("td");
+            terminArt.classList.add("col-3");
+            tr.appendChild(terminArt);
+            terminArt.innerHTML = termin.art;
 
-        let terminAusstehend = document.createElement("td");
-        terminAusstehend.classList.add("col-3");
-        tr.appendChild(terminAusstehend);
-        terminAusstehend.innerHTML = "---";
+            let terminAusstehend = document.createElement("td");
+            terminAusstehend.classList.add("col-3");
+            tr.appendChild(terminAusstehend);
+            terminAusstehend.innerHTML = "---";
 
-        let terminArzt = document.createElement("td");
-        terminArzt.classList.add("col-3");
-        tr.appendChild(terminArzt);
-        terminArzt.innerHTML = termin.arzt;
+            let terminArzt = document.createElement("td");
+            terminArzt.classList.add("col-3");
+            tr.appendChild(terminArzt);
+            terminArzt.innerHTML = termin.arzt;
+        }
     }
 }
 
-function loescheTabelle() {
+function loescheTerminTabelle() {
     let table = document.getElementById("terminDaten");
     table.innerHTML = "";
 }
 
-function holeTermine() {
+function loescheAusstehendTabelle() {
+    let table = document.getElementById("ausstehendDaten");
+    table.innerHTML = "";
+}
+
+//! Hier muss das StorageKey mit .... (z.B. ausstehend) wieder geändert werden, wenn der Impfpass implementiert wird
+//! Namens-Konvention beachten!
+function holeLocalStorage() {
     for (let i = 0; i < localStorage.length; i++) {
         let storageKey = localStorage.key(i);
         if(storageKey.slice(0,6) == "termin")
             termine.push(JSON.parse(window.localStorage.getItem(storageKey)));
+        /*if(storageKey.slice(0,10) == "ausstehend")
+            ausstehendeImpfungen.push(JSON.parse(window.localStorage.getItem(storageKey)));*/
     }
 }
 
@@ -99,20 +112,24 @@ function speichereTermine() {
 function abgeschlossenAuslesen(){
     let abgeschlossen = document.getElementsByName("abgeschlossenEintrag")
     for(let part of abgeschlossen){
-        let datum = part.childNodes[1].textContent;
-        let art = part.childNodes[3].textContent;
-        let charge = part.childNodes[5].textContent;
-        let arzt = part.childNodes[7].textContent;
+        var ausstehend = new Object();
+
+        ausstehend.datum = part.childNodes[1].textContent;
+        ausstehend.art = part.childNodes[3].textContent;
+        ausstehend.charge = part.childNodes[5].textContent;
+        ausstehend.arzt = part.childNodes[7].textContent;
 
         let datumHeute = new Date()
-        console.log(datum)
-        let datumImpfung = parseDate(datum)
+        console.log(ausstehend.datum)
+        let teileImpfdatum = parseDate(ausstehend.datum)
+        datumHeute = new Date(datumHeute)
+        let datumImpfung = new Date(teileImpfdatum[2], teileImpfdatum[1]-1, teileImpfdatum[0]);
 
-        //let datum
         console.log(datumHeute);
         console.log(datumImpfung)
-        datumHeute = new Date(datumHeute)
-        datumImpfung = new Date(datumImpfung)
+  
+        //datumImpfung = new Date(datumImpfung)
+        //let datumnaechsteImpfung = new Date((datum))
 
         let zeitDifferenz = datumHeute - datumImpfung;
         console.log(zeitDifferenz)
@@ -123,21 +140,96 @@ function abgeschlossenAuslesen(){
 
         console.log(tage)
         console.log(jahre)
-        if(art == "Grippe"){
-            if(jahre > 1){
-                speichereAusstehend();
-            }
+        // HIER erfolgt die Zeitberechnung der Wiederholung von Impfugen nach einem bestimmten Zeitraum
+        // Bei dem verschiedenen Impfungen muss das Jahr als Zeitabstand zwischen den Impfungen angepasst werden!
+        switch(ausstehend.art.trim()){
+            case "Grippe":
+                if(jahre >= 1){
+                    ausstehend.naechsteImpfung = new Date(parseInt(teileImpfdatum[2])+1, teileImpfdatum[1]-1, teileImpfdatum[0]).toLocaleDateString('de-DE');
+                    console.log("Nächste Impfung: " + ausstehend.naechsteImpfung)
+                    console.log(ausstehend)
+                    ausstehendeImpfungen.push(ausstehend);
+                }
+                break;
+            case "Kombi":
+                if(jahre >= 10){
+                    ausstehend.naechsteImpfung = new Date(parseInt(teileImpfdatum[2])+10, teileImpfdatum[1]-1, teileImpfdatum[0]).toLocaleDateString('de-DE');
+                    console.log(ausstehend)
+                    ausstehendeImpfungen.push(ausstehend);
+                }
+                break;
         }
     }
+    speichereAusstehend();
 }
+
+//TODO Diese Methode kann gelöscht werden, wenn die Daten aus dem Impfpass genommen werden
+
 function speichereAusstehend(){
-    console.log()
+    let zaehlerAusstehend = 0;
+    for(let ausstehend of ausstehendeImpfungen){
+        let key = "ausstehend" + zaehlerAusstehend;
+        let value = JSON.stringify(ausstehendeImpfungen[zaehlerAusstehend]);
+        console.log(key + " :" + value);
+        window.localStorage.setItem(key, value);
+        zaehlerAusstehend = zaehlerAusstehend + 1;    
+    }
+    zeichneAusstehend();
+}
+
+function zeichneAusstehend(){
+    loescheAusstehendTabelle();
+
+    let table = document.getElementById("ausstehendDaten");
+    if (ausstehendeImpfungen == "") {
+        let tr = document.createElement("tr");
+        tr.classList.add("d-flex");
+        table.appendChild(tr);
+
+        let ausstehendDatum = document.createElement("td");
+        ausstehendDatum.classList.add("col-12");
+        tr.appendChild(ausstehendDatum);
+        terminDatum.innerHTML = "Keine ausstehenden Impfungen";
+    }else{
+
+        for (let ausstehend of ausstehendeImpfungen) {
+            let tr = document.createElement("tr");
+            tr.classList.add("d-flex");
+            table.appendChild(tr);
+
+            let ausstehendDatum = document.createElement("td");
+            ausstehendDatum.classList.add("col-3");
+            tr.appendChild(ausstehendDatum);
+            ausstehendDatum.innerHTML = ausstehend.naechsteImpfung;
+
+            let ausstehendArt = document.createElement("td");
+            ausstehendArt.classList.add("col-3");
+            tr.appendChild(ausstehendArt);
+            ausstehendArt.innerHTML = ausstehend.art;
+
+            ausstehendDatum = document.createElement("td");
+            ausstehendDatum.classList.add("col-3");
+            tr.appendChild(ausstehendDatum);
+            ausstehendDatum.innerHTML = ausstehend.datum;
+
+            let ausstehendBuchung = document.createElement("td");
+            ausstehendBuchung.classList.add("col-3");
+            tr.appendChild(ausstehendBuchung);
+            let buchenButton = document.createElement("a");
+            buchenButton.classList.add("btn");
+            buchenButton.classList.add("btn-outline-warning");
+            buchenButton.setAttribute("href","#");
+            buchenButton.setAttribute("role","button");
+            ausstehendBuchung.appendChild(buchenButton);
+            buchenButton.innerHTML = "Termin buchen"
+        }
+    }    
 }
 //Quelle: https://stackoverflow.com/questions/2945113/how-to-create-a-new-date-in-javascript-from-a-non-standard-date-format/2945150
 function parseDate(input) {
     var parts = input.match(/(\d+)/g);
-    // note parts[1]-1
-    return new Date(parts[2], parts[1]-1, parts[0]);
+    //console.log(parts);
+    return parts;
   }
 /*
 function setzeAktuelleZeit(){
