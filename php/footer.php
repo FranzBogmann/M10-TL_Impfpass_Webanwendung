ca5d0ccdeec9824778e94afcae6828b169a76f2d
FranzBogmann <67625001+FranzBogmann@users.noreply.github.com>
2021-06-02 16:58:56 +0200

    Footer Text angepasst

--- php/footer.php
+++ php/footer.php
@@ -3,10 +3,7 @@
                 <p class="float-end"><a class="text-dark" href="#">Nach Oben⬆</a>
                                      <a class="text-dark" id="exportJson">Export JSON</a></p>
                 <p class="text-dark"><a class="text-dark"
-                        href="https://vit-bund.de">Studiengang
-                        Verwaltungsinformatik</a> | <a class="text-dark"
-                        href="https://github.com/FranzBogmann/M10-TL_Impfpass_Webanwendung">Gruppe
-                        Impfpass</a></p>
-
+                        href="https://vit-bund.de">Studiengang VIT</a> | <a class="text-dark"
+                        href="https://github.com/FranzBogmann/M10-TL_Impfpass_Webanwendung">Team 2.09: Digitaler Impfpass</a></p>
             </div>
 </footer>
