5c936b48096115abc23b860bf44b3825064a2f1b
FranzBogmann <67625001+FranzBogmann@users.noreply.github.com>
2021-06-03 20:20:25 +0200

    Anleitung auf index-Seite ergänzt

--- index.php
+++ index.php
@@ -10,33 +10,32 @@
 
         <div class="container">
             <div class="row">
-                <p>Herzlich Willkommen bei deinem digitalen Impfpass😉</p>
-                <p>Diese Webanwendung erlaubt dir deine Impfungen digital zu erfassen. So hast Du deinen Imfpass immer dabei!👌</p>
+                <p>Willkommen bei dem digitalen Impfpass💉</p>
+                <p>Diese Webanwendung erlaubt dir alle deine vergangenen Impfungen digital zu erfassen sowie zukünftige Impftermine zu speichern. So hast Du deinen Imfpass immer dabei und verlierst nie den Überblick über alle Impfungen!😉</p>
             </div>
 
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="card">
-                      <img src="images/pexels/ImpfpassHandy.jpg" class="card-img-top" alt="Digitaler Impfpass">
-                      <div class="card-body">
-                        <h5 class="card-title">Impfungen eintragen</h5>
-                        <p class="card-text">Digitalisiere Deine Impfungen durch neue Impfeinträge in den digitalen Impfpass!</p>
-                        <a href="Impfpass.php" class="btn btn-primary">Zum Digitalen Impfpass</a>
+              <div class="col-lg-6">
+                      <div class="card">
+                        <img src="images/pexels/ImpfpassHandy.jpg" class="card-img-top" alt="Digitaler Impfpass">
+                        <div class="card-body">
+                          <h5 class="card-title">Impfungen eintragen</h5>
+                          <p class="card-text">Digitalisiere Deine Impfungen durch neue Impfeinträge in den digitalen Impfpass!</p>
+                          <a href="Impfpass.php" class="btn btn-primary">Zum Digitalen Impfpass</a>
+                        </div>
                       </div>
+                  </div>
+                  <div class="col-lg-6">
+                        <div class="card">
+                          <div class="card-body">
+                            <h5 class="card-title">Anleitung zur Nutzung</h5>
+                            <p class="card-text">Der digitale Impfpass besteht aus drei Bereichen: Einem Nutzerprofil, einer Terminübersicht und dem Impfpass selbst.</p>
+                          </div>
+                          <img src="images/index/Tutorial.png" class="card-img-bottom" alt="Anleitung zum Impferfolg">
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card">
-                      <img src="..." class="card-img-top" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                    </div>
-                </div>  
+              </div>
             </div>
-        </div>
         <br>
         
         <!-- Hier endet der Inhalt der Seite-->
--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,8 @@
 <footer class="footer mt-auto py-3 bg-warning text-white">
             <div class="container">
-                <p class="float-end"><a class="text-dark" href="#">Nach Oben⬆</a>
-                                     <a class="text-dark" id="exportJson">Export JSON</a></p>
+                <p class="float-end"><a class="text-dark" id="exportJson">Export Impfpass als JSON-Datei</a>
+                                     <a class="text-dark">|</a>
+                                     <a class="text-dark" href="#">Nach Oben⬆</a></p>
                 <p class="text-dark"><a class="text-dark"
                         href="https://vit-bund.de">Studiengang VIT</a> | <a class="text-dark"
                         href="https://github.com/FranzBogmann/M10-TL_Impfpass_Webanwendung">Team 2.09: Digitaler Impfpass</a></p>
