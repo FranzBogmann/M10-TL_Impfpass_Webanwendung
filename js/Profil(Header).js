277e95b4aab1f82ee490e8e12ebb276fbc27b81e
JockerLukas <62982416+Lukas-S2000@users.noreply.github.com>
2021-06-05 13:23:51 +0200

    Überprüfung der Email im Profil
    Sollte die Email im Profil nicht korrekt eingegeben worden sein wird dies nun per alert dargesdtellt und der bearbeitungsmodus wird wieder eingschaltet

--- js/Profil(Header).js
+++ js/Profil(Header).js
@@ -4,7 +4,9 @@ window.addEventListener("load", init);
 function init() {
     //Änderung des Bearbeitungsmodus von gucken zu bearbeiten bei click auf bearbeiten Button
     document.getElementById("bearbeiten").addEventListener("click", bearbeitungsmodus);
-    //Überprüfen ob schon ein Profil vorhanden ist fals nicht wird ein Standartprofil angelegt mit dem ersteinmal gearbeitet werden kann
+    //Variable die den Status bearbeitungsstatus des Modals wiederspiegelt
+    let modus = 0;
+    //Überprüfen ob schon ein Standartprofil vorhanden ist fals nicht wird ein Standartprofil angelegt mit dem ersteinmal gearbeitet werden kann
     if (typeof standartProfil === "undefined")
         erstelleStandartProfil();
     if (typeof standartArzt === "undefined")
@@ -72,7 +74,6 @@ function profilBildÄnderung() {
                 let key = "Bild";
                 let value = JSON.stringify(bild);
 
-                console.log(key + " :" + value);
                 window.localStorage.setItem(key, value);
                 profilIconÄndern();
             });
@@ -113,22 +114,27 @@ function profilaufruf() {
 
 function bearbeitungsmodus() {
     let inputs = document.getElementById("Profilepage").getElementsByTagName("input");
+    
     //Änderung der Inputfelder von Disabled zu Abled(?) damit diese bearbeitet werden können
     for (let inputfelder of inputs) {
         if (inputfelder.disabled == false) {
             inputfelder.disabled = true;
+            modus = 1;
         }
         else {
             inputfelder.disabled = false;
+            modus = 0;
         }
     }
     //Der Button ändert sein Text je nachdem in welchem Modus er ist
     let buttonname = document.getElementById("bearbeiten");
     if (buttonname.innerHTML == "Bearbeiten") {
         buttonname.innerHTML = "Speichern";
+        
     }
     else {
         buttonname.innerHTML = "Bearbeiten";
+        
     }
 }
 
@@ -140,21 +146,26 @@ function profilSpeichern() {
 
     //Abfrage ob ein Wert exestiert und folglich dessen Speicherung im Object Profil
     if (document.getElementById("vorname").value != null)
-        profil.vorname = document.getElementById("vorname").value; console.log(document.getElementById("vorname").value);
+        profil.vorname = document.getElementById("vorname").value;
     if (document.getElementById("nachname").value != null)
-        profil.nachname = document.getElementById("nachname").value; console.log(document.getElementById("nachname").value);
+        profil.nachname = document.getElementById("nachname").value;
     if (document.getElementById("email").value != null)
-        profil.email = document.getElementById("email").value; console.log(document.getElementById("email").value);
+        profil.email = document.getElementById("email").value;
     if (document.getElementById("geburtsdatum").value != null)
-        profil.geburtstag = document.getElementById("geburtsdatum").value; console.log(document.getElementById("geburtsdatum").value);
+        profil.geburtstag = document.getElementById("geburtsdatum").value;
     if (document.getElementById("wohnsitz").value != null)
-        profil.wohnsitz = document.getElementById("wohnsitz").value; console.log(document.getElementById("wohnsitz").value);
+        profil.wohnsitz = document.getElementById("wohnsitz").value;
+
+    //Überprüft ob die Email richitg eingegben ist sollte dies nicht der Fall sein wird der Bearbeitungsmodus einfach erneut eingeschaltet    
+    if(modus == 1){  
+        ValidateEmail(profil.email);
+        bearbeitungsmodus();
+    } 
 
     // Schreiben des Profils in ein JSON Object    
     let key = "Profil";
     let value = JSON.stringify(profil);
 
-    console.log(key + " :" + value);
     window.localStorage.setItem(key, value);
 
     //Änderung des Names der oben im Profil angezeigt wird
@@ -165,14 +176,13 @@ function arztSpeichern() {
     //Selber Vorgang wie bei profilSpeichern
     let arzt = new Object();
     if (document.getElementById("hausarzt").value != null)
-        arzt.hausarzt = document.getElementById("hausarzt").value; console.log(document.getElementById("hausarzt").value);
+        arzt.hausarzt = document.getElementById("hausarzt").value;
     if (document.getElementById("hausarztTelefonnummer").value != null)
-        arzt.hausarztTelefonnummer = document.getElementById("hausarztTelefonnummer").value; console.log(document.getElementById("hausarztTelefonnummer").value);
+        arzt.hausarztTelefonnummer = document.getElementById("hausarztTelefonnummer").value;
 
     let key = "Arzt";
     let value = JSON.stringify(arzt);
 
-    console.log(key + " :" + value);
     window.localStorage.setItem(key, value);
 }
 
@@ -208,4 +218,13 @@ function erstelleStandartArzt() {
 
     console.log(key + " :" + value);
     window.localStorage.setItem(key, value);
+}
+
+function ValidateEmail() {
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test( document.getElementById("email").value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
 }
