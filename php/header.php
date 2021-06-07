62ceefe710d61cb379a7fa13e55b366522371ad1
lcsnpmk <67631559+lcsnpmk@users.noreply.github.com>
2021-06-07 14:48:49 +0200

    kurzer relativ Pfad-Fix im Header

--- php/header.php
+++ php/header.php
@@ -4,7 +4,7 @@
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <link rel='stylesheet' href='css/Bootstrap 5/bootstrap.min.css'>
         <!-- Favicon made by "catkuro" "https://www.flaticon.com/" -->
-        <link rel="icon" type="image/svg+xml" href="/images/icons/favicon.svg"
+        <link rel="icon" type="image/svg+xml" href="./images/icons/favicon.svg"
             sizes="any">
         <meta name="author" content="Franz Bogmann; Lucas Gebert; David Buchert;
             Lukas Siegburg: Teilleistung Modul Webtechnologien, Studiengang
